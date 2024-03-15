89d44b0d7bdb41a8d2f60f725fbec25416ab5801
AneelaTasneem <aneelatasneem90@gmail.com>
2024-03-15 09:05:01 -0700

    first commit

---
+++ bmi.js
@@ -0,0 +1,10 @@
+"use strict";
+// BMI Calculator:
+// creating a variable of weight in KG:
+let weightInKg = 66;
+// Creating a variable of height in meters:
+let heightInMeters = 1.60;
+// Creatin a variable of BMI:
+let BMI = weightInKg / (heightInMeters * heightInMeters);
+// Printing BMI:
+console.log(`" Your BMI is ${BMI}`);
---
+++ bmi.ts
@@ -0,0 +1,13 @@
+// BMI Calculator:
+
+// creating a variable of weight in KG:
+let weightInKg = 66; 
+
+// Creating a variable of height in meters:
+let heightInMeters= 1.60; 
+
+// Creatin a variable of BMI:
+let BMI = weightInKg/( heightInMeters *heightInMeters);
+
+// Printing BMI:
+console.log(`" Your BMI is ${BMI}`);
